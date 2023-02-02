73df4a777ea073d92e54a393d6951eb34296102a
Fran3003 <franco.rantuchoQgmail.com>
2023-02-02 11:52:34 -0300

    deploy

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const projects = [
+  {
+    title: "Henry Dogs",
+    subtitle: "Proyecto individual",
+    description:
+      "Es una Single-page application que consume información de theDogApi, permitiéndonos además crear nuestras propias razas de perros. Tecnologias usadas: JavaScript, React.js, Redux, CSS, HTML, Node.js, Sequelize, PostgreSQL, Express.js",
+    image: "./HenryDogs.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "EasyLearning",
+    subtitle: "Proyecto final grupal",
+    description:
+      "Plataforma de e-learning donde el usuario puede comprar, crear y vender sus propios cursos. Se utilizaron las siguientes tecnologías: JavaScript, React.js, Redux, Node.js, PostgreSQL, Sequelize, Express.js, Chakra UI.",
+    image: "./easyLearning.png",
+    link: "https://easylearning-fp.web.app/",
+  },
+  // {
+  //   title: "DevChat",
+  //   subtitle: "React and Firebase",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-3.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+  // {
+  //   title: "Epic Todo App",
+  //   subtitle: "React Hooks",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-4.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+];
+
+export const testimonials = [
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/1.jpg",
+//     name: "Jesse Hicks",
+//     company: "Zoozle",
+//   },
+//   {
+//     quote:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+//     image: "https://randomuser.me/api/portraits/men/94.jpg",
+//     name: "Ruben Alvarez",
+//     company: "Dooble.io",
+//   },
+];
+
+export const skills = [
+  "JavaScript",
+  "React.js",
+  "Redux",
+  "NodeJS",
+  "Express.js",
+  "Sequelize",
+  "PostgresSQL",
+  "Git",
+  "HTML5",
+  "CSS"
+];
